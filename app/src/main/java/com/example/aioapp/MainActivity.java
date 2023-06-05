0e4a783c3efa8ba0f4a7212d96b250b359952d03
jedddi <jedquimno7@gmail.com>
2023-06-05 18:18:30 +0800

    created bottom sheet for adding task

--- app/src/main/java/com/example/aioapp/MainActivity.java
+++ app/src/main/java/com/example/aioapp/MainActivity.java
@@ -7,9 +7,12 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.Calendar;
 
@@ -18,27 +21,39 @@ import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button addlistbtn;
-    Dialog mDialog;
+
 
     private HorizontalCalendar horizontalCalendar;
+    private Button add_list_button1;
+    private BottomSheetDialog add_list_bottomsheet;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do_list_page);
 
-        ImageButton addlistbtn = findViewById(R.id.addlistbtn);
-        mDialog = new Dialog(this);
-
-        addlistbtn.setOnClickListener(new View.OnClickListener() {
+// Dialog Code
+        ImageButton add_list_button1=findViewById(R.id.addlistbtn);
+        add_list_button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDialog.setContentView(R.layout.add_new_list_popup);
-                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                mDialog.show();
+                add_list_bottomsheet=new BottomSheetDialog(MainActivity.this, R.style.bottom_sheet_theme);
+                View sheetview= LayoutInflater.from(getApplicationContext()).inflate(R.layout.add_list_bottomsheet, null);
+
+                sheetview.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        add_list_bottomsheet.dismiss();
+                    }
+                });
+
+                add_list_bottomsheet.setContentView(sheetview);
+                add_list_bottomsheet.show();
+
             }
         });
-
+//Calendar Code
         /* starts before 1 month from now */
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.MONTH, -1);
