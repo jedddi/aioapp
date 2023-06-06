365a3715d11ee230e0cc533aa29325df96ab8451
Bigbossian <cruzaianjames15@gmail.com>
2023-06-06 21:01:02 +0800

    new mainactivity

--- app/src/main/java/com/example/aioapp/MainActivity.java
+++ app/src/main/java/com/example/aioapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.aioapp;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.relativeLayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(v -> openDashboard());
