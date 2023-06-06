637d231b4d0c2c2feac1de7d4e84c19608999b31
Bigbossian <cruzaianjames15@gmail.com>
2023-06-06 23:55:38 +0800

    new mainactivity

--- app/src/main/java/com/example/aioapp/MainActivity.java
+++ app/src/main/java/com/example/aioapp/MainActivity.java
@@ -4,16 +4,50 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.Button;
-
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the scale up animation
+        ObjectAnimator scaleUpX = ObjectAnimator.ofFloat(imageView, "scaleX", 1f, 1.2f);
+        scaleUpX.setDuration(5000);
+        scaleUpX.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleUpX.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleUpX.setRepeatMode(ObjectAnimator.REVERSE);
+
+        ObjectAnimator scaleUpY = ObjectAnimator.ofFloat(imageView, "scaleY", 1f, 1.2f);
+        scaleUpY.setDuration(5000);
+        scaleUpY.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleUpY.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleUpY.setRepeatMode(ObjectAnimator.REVERSE);
+
+        // Create the fade in animation
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(imageView, "alpha", 0f, 1f);
+        fadeIn.setDuration(5000);
+        fadeIn.setInterpolator(new AccelerateDecelerateInterpolator());
+        fadeIn.setRepeatCount(ObjectAnimator.INFINITE);
+        fadeIn.setRepeatMode(ObjectAnimator.REVERSE);
+
+        // Combine the scale up and fade in animations into a set
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(scaleUpX, scaleUpY, fadeIn);
+
+        // Start the animation
+        animatorSet.start();
+
 
         ConstraintLayout constraintLayout = findViewById(R.id.relativeLayout);
 
