17aac239d4a69d79148f16d88576e8aaf1e10183
Bigbossian <cruzaianjames15@gmail.com>
2023-07-01 18:41:47 +0800

    update About app and about us

--- app/src/main/java/com/example/aioapp/NotificationActivity.java
+++ app/src/main/java/com/example/aioapp/NotificationActivity.java
@@ -5,15 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NotificationActivity extends AppCompatActivity {
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        backButton = findViewById(R.id.returnDash);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back_Dashboard = new Intent(NotificationActivity.this, Dashboard_activity.class);
+                startActivity(back_Dashboard);
+            }
+        });
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
